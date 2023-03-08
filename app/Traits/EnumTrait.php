cf0c1eebef48b01703cded00f4264a6b7e43bfca
veerman72 <64311955+veerman72@users.noreply.github.com>
2023-03-08 12:44:41 +0100

    refactoring EnumTrait.php

--- app/Traits/EnumTrait.php
+++ app/Traits/EnumTrait.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Collection;
 
 trait EnumTrait
 {
+    public function get(): Collection
+    {
+        return $this->getItem();
+    }
+
     public static function all(): Collection
     {
         return self::getCollection();
@@ -19,14 +24,17 @@ trait EnumTrait
     private static function getCollection(): Collection
     {
         return collect(self::cases())
-            ->map(
-                fn ($item) => [
-                    'id' => $item->value,
-                    'name' => $item->name,
-                    'description' => $item->description(),
-                ],
-            )
+            ->map(fn ($item) => $item->getItem())
             ->sortBy('description')
             ->values();
+    }
+
+    private function getItem(): Collection
+    {
+        return collect([
+            'id' => $this->value,
+            'name' => $this->name,
+            'description' => $this->description(),
+        ]);
     }
 }
